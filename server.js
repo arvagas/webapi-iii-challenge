367ed680503b70b3496374e166e2965e2576bdf2
Arvin Agas <arvagas@gmail.com>
2019-09-04 11:58:31 -0400

    created logger middleware

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 })
 
-//custom middleware
-
+// @@@@@@@@@@ Global Middleware @@@@@@@@@@
 function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`)
 
+  next()
 }
+
+server.use(logger)
 
 module.exports = server
